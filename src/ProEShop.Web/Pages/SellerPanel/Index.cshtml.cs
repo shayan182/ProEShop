873ca88e91a7809bfbfd05056428fc179586fcf5
Shayan Tohidi <182shayan@gmail.com>
2022-08-13 16:00:24 +0430

    Create Product (Seller panel js)

--- src/ProEShop.Web/Pages/SellerPanel/Index.cshtml.cs
+++ src/ProEShop.Web/Pages/SellerPanel/Index.cshtml.cs
@@ -1,11 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace ProEShop.Web.Pages.SellerPanel;
 
 public class IndexModel : SellerPanelBase
 {
     public void OnGet()
     {
+       
     }
 }
